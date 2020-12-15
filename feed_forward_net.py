6fa1f24c0f132fad9ecb2b3fd7eb6c426349ddd2
Grainne Casey <g_r_casey@live.com>
2020-12-15 15:25:17 -0500

    feed forward nn cross validation

--- feed_forward_net.py
+++ feed_forward_net.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import plot_roc_curve, roc_auc_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import (
     confusion_matrix,
     classification_report,
@@ -52,10 +53,35 @@ x_train, x_test, y_train, y_test = train_test_split(X_scaled_df, Y, test_size=.2
 max_depth = [1, 5, 10, 15]
 
 
-nn = MLPClassifier().fit(x_train, y_train)
 
-test_pred = nn.predict(x_test)
-train_pred = nn.predict(x_train)
+
+# Cross Validation
+param_grid = {'hidden_layer_sizes': [(10, 30, 10), (100,), (150,)],
+              'solver': ['sgd', 'adam'],
+              'alpha': [0.0001, 0.05],
+              'max_iter': [200, 300],
+              'learning_rate': ['constant', 'adaptive']
+              }
+
+nn = MLPClassifier()
+
+nn_grid_search_cv = GridSearchCV(nn, param_grid, cv=5,
+                                 scoring="accuracy",
+                                 return_train_score=True,
+                                 verbose=True,
+                                 n_jobs=-1)
+
+nn_grid_search_cv.fit(x_train, y_train)
+
+print("CV best params:")
+print(nn_grid_search_cv.best_params_)
+print("CV best estimator")
+print(nn_grid_search_cv.best_estimator_)
+
+# Testing
+
+test_pred = nn_grid_search_cv.predict(x_test)
+train_pred = nn_grid_search_cv.predict(x_train)
 test_acc = (accuracy_score(test_pred, y_test))
 test_error = 1 - test_acc
 train_acc = (accuracy_score(train_pred, y_train))
@@ -76,5 +102,5 @@ print("Test Precision:", test_prec)
 print("-----------------------------------------")
 print("Testing classification report:")
 print(classification_report(y_test, test_pred, labels=genres_master_list))
-plot_confusion_matrix(nn, x_test, y_test, labels=genres_master_list, xticks_rotation='vertical')
+plot_confusion_matrix(nn_grid_search_cv, x_test, y_test, labels=genres_master_list, xticks_rotation='vertical')
 plt.show()
